cd0f550a94e7e5342fcd2ab456440f7198f6956b
sergei.doroshenko <6823298@gmail.com>
2016-08-17 13:53:25 +0300

    Created tests

---
+++ run.bat
@@ -0,0 +1,1 @@
+java -jar ./target/sb-test-1.0.jar
--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,6 +2,9 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 /**
  * Created by Sergei on 16.08.2016.
@@ -10,6 +13,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+//        SpringApplication.run(Application.class, args);
+
+        ApplicationContext ctx = SpringApplication.run( Application.class, args );
+
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
     }
 }
